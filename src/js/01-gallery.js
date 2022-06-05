ccd7a78f9d1f4b2d25daf7e00250befe176ce72d
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-05 18:45:37 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,3 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
