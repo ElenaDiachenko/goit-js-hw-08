849b64e4843f1e78a1cb96102117a18493771a5e
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-05 19:41:54 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,26 @@
-import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const containerRef = document.querySelector('.gallery');
+
+containerRef.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) {
+  e.preventDefault();
+  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+  
+}
+
+const makeItemMarkup = item => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+  `
+}
+const makeAllItemMarkup = galleryItems.map(makeItemMarkup)
+  .join('\n');
+
+containerRef.insertAdjacentHTML('beforeend', makeAllItemMarkup)
